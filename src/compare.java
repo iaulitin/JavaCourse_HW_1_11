b8268defa639e7570515ae162aeca7d48b6a69ff
Frolov Georgy <garry96.96@yandex.ru>
2021-12-29 18:38:50 +0300

    fixed

--- src/compare.java
+++ src/compare.java
@@ -2,6 +2,6 @@ import java.sql.SQLException;
 
 public class compare {
     public static void main(String[] args) throws SQLException {
-        System.out.println(dataBase.compareTables("Dogs1", "Dogs2", "dogs", "dogs2", "name", "color", "owner_id"));
-    }
+        System.out.println(DataBase.compareTables("postgres", "postgres", "Dogs1", "Dogs2", "dogs", "dogs2", "owner_id", "name", "color"));
+        }
 }
